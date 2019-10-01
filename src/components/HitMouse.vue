55a5c4717d22e837ec2720300998ee21ceea3b24
unknown <blue520gt@126.com>
2019-10-02 02:05:42 +0800

    2 AM

--- src/components/HitMouse.vue
+++ src/components/HitMouse.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="HitMouse" id="HitMouse">
     <div id="changeMouse" class="changeMouse" @mousedown="mouseDown()" @mouseup="mouseUp()">
-      <div style="display: inline">
-        <div class="mouseA1"></div>
-        <div class="mouseA2"></div>
-        <div class="mouseA3"></div>
-        <div class="mouseA4"></div>
+      <div class="mouseTop">
+        <img class="mouse" @click="hitMouse()" id="mouse1" src="../assets/mouseNone.png" />
+        <img class="mouse" @click="hitMouse()" id="mouse2" src="../assets/mouseNone.png" />
+        <img class="mouse" @click="hitMouse()" id="mouse3" src="../assets/mouseNone.png" />
+        <img class="mouse" @click="hitMouse()" id="mouse4" src="../assets/mouseNone.png" />
       </div>
-      <div style="display: inline">
-        <div class="mouseB1"></div>
-        <div class="mouseB2"></div>
-        <div class="mouseB3"></div>
-        <div class="mouseB4"></div>
+      <div class="mouseBottom">
+        <img class="mouse" @click="hitMouse()" id="mouse5" src="../assets/mouseNone.png" />
+        <img class="mouse" @click="hitMouse()" id="mouse6" src="../assets/mouseNone.png" />
+        <img class="mouse" @click="hitMouse()" id="mouse7" src="../assets/mouseNone.png" />
+        <img class="mouse" @click="hitMouse()" id="mouse8" src="../assets/mouseNone.png" />
       </div>
     </div>
+    <p>hitMouseCount:{{hitMouseCount}}</p>
   </div>
 </template>
 
 <script>
 import Vue from "Vue";
 // import changeMouseA from "../assets/mouseOver.png";
+import mouseOnce from "../assets/mouseOnce.gif";
+import mouseNone from "../assets/mouseNone.png";
 export default {
   name: "HitMouse",
   //status == 0: 老鼠未出现
@@ -27,8 +30,21 @@ export default {
   //status == 2: 老鼠被锤到并下降
   data() {
     return {
-      status: 0
+      //status代表老鼠当前的状态:
+      // 0:未播放gif
+      // 1：正在播放gif
+      // 2：用户点击到了老鼠后，切换为被砸了并降落的gif
+      status: [0, 0, 0, 0, 0, 0, 0, 0],
+      hitMouseCount: 0,
+      mouseOnce,
+      interval: []
     };
+  },
+  mounted() {
+    this.mouseAppear();
+    // setInterval(() => {
+    //   this.initGame();
+    // }, 2000);
   },
   methods: {
     mouseDown: function() {
@@ -37,6 +53,44 @@ export default {
     },
     mouseUp: function() {
       document.getElementById("changeMouse").className = "changeMouse";
+    },
+    hitMouse: function() {
+      // if (1 == this.status) {
+      this.hitMouseCount++;
+      //   this.status[this.location] = 2;
+      // }
+    },
+    mouseAppear: function() {
+      // 1.设置定时器，等待2s
+      // 2.八个点随机一个
+      // 3.播放gif
+      // 4.等2s后，在剩余的七个点中随机一个
+      // var locations = [1, 2, 3, 4, 5, 6, 7, 8];
+      //Math.random():获取0~1随机数;Math.floor取整;
+      //var location = locations[Math.floor(Math.random() * locations.length)];
+      var locations = [];
+      for (var i = 0; i < this.status.length; i++) {
+        if (0 == this.status[i]) {
+          locations.push(i + 1);
+        }
+      }
+      console.log("status:" + this.status);
+      console.log("locations:" + locations);
+      var location = locations[Math.floor(Math.random() * locations.length)];
+      var self = this;
+      var randomTime = Math.floor(Math.random() * (3000 - 1000 + 1) + 1000);
+      setTimeout(function() {
+        document.getElementById("mouse" + location).src = mouseOnce;
+        self.status[location - 1] = 1;
+        setTimeout(function() {
+          document.getElementById("mouse" + location).src = mouseNone;
+          self.status[location - 1] = 0;
+          console.log("第" + location + "只老鼠回去了");
+        }, 3000);
+        self.mouseAppear();
+      }, randomTime);
+      console.log("第" + location + "只老鼠");
+      console.log("随机的时间" + randomTime);
     }
     // mouseOver: function() {
     //   this.style.cursor = "url(../assets/mouseOver.png),auto";
@@ -87,82 +141,22 @@ export default {
   cursor: url(../assets/mouseDown.png), auto;
   margin: auto;
 }
-.mouseA1 {
-  width: 78px;
+.mouseTop {
   height: 78px;
-  /* position: absolute; */
-  position: relative;
-  left: 65px;
-  top: 179px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
-  /* display: inline; */
+  width: 100%;
+  padding-top: 179px;
 }
-.mouseA2 {
-  width: 78px;
+.mouseBottom {
   height: 78px;
-  /* position: absolute; */
-  position: relative;
-  left: 190px;
-  top: 179px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
+  width: 100%;
+  padding-top: 4px;
 }
-.mouseA3 {
+.mouse {
   width: 78px;
   height: 78px;
-  /* position: absolute; */
-  position: relative;
-  left: 315px;
-  top: 179px;
-  background-image: url(../assets/mouse.gif);
+  /* position: relative; */
   float: left;
-}
-.mouseA4 {
-  width: 78px;
-  height: 78px;
-  /* position: absolute; */
-  position: relative;
-  left: 440px;
-  top: 179px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
-}
-.mouseB1 {
-  width: 78px;
-  height: 78px;
-  /* position: absolute; */
-  position: relative;
-  left: -248px;
-  top: 261px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
-}
-.mouseB2 {
-  width: 78px;
-  height: 78px;
-  position: relative;
-  left: -123px;
-  top: 261px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
-}
-.mouseB3 {
-  width: 78px;
-  height: 78px;
-  position: relative;
-  left: 2px;
-  top: 261px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
-}
-.mouseB4 {
-  width: 78px;
-  height: 78px;
-  position: relative;
-  left: 127px;
-  top: 261px;
-  background-image: url(../assets/mouse.gif);
-  float: left;
+  margin-left: 65px;
+  margin-right: 60px;
 }
 </style>
---
+++ src/components/test.js
@@ -0,0 +1,6 @@
+var locations = [];
+for (var i = 0; i < status.length; i++) {
+  if (0 == status[i]) {
+    locations.push(i + 1);
+  }
+}
